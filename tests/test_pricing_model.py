14bcbac1dcea9148215993ecf48dc025fd39186b
Nattawut Boonnoon <leonardo_90s@hotmail.com>
2026-07-02 21:09:18 +0700

    Update test_pricing_model.py

--- tests/test_pricing_model.py
+++ tests/test_pricing_model.py
@@ -32,18 +32,3 @@ def test_put_call_parity_multiple_cases() -> None:
         left_side = call + case["K"] * np.exp(-case["r"] * case["T"])
         right_side = put + case["S"]
         assert pytest.approx(left_side, abs=1e-2) == right_side
-
-def test_option_strategy_payoff() -> None:
-    
-    stock_range = np.array([90.0, 100.0, 110.0])
-    K = 100.0
-    call_premium = 5.0
-    put_premium = 4.0
-    
-    
-    lc_payoff = BlackScholesModel.option_strategy_payoff("Long Call", stock_range, K, call_premium, put_premium)
-    np.testing.assert_array_almost_equal(lc_payoff, [-5.0, -5.0, 5.0])
-
-
-    straddle_payoff = BlackScholesModel.option_strategy_payoff("Long Straddle", stock_range, K, call_premium, put_premium)
-    np.testing.assert_array_almost_equal(straddle_payoff, [1.0, -9.0, 1.0])

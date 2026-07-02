dcd0e5530352fe1792f218727752a4b1b8caea97
Nattawut Boonnoon <leonardo_90s@hotmail.com>
2026-07-02 16:15:43 +0700

    Update test_pricing_model.py

--- tests/test_pricing_model.py
+++ tests/test_pricing_model.py
@@ -1,5 +1,9 @@
 import pytest
 import numpy as np
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from src.pricing_model import BlackScholesModel
 
 def test_textbook_reference_value() -> None:

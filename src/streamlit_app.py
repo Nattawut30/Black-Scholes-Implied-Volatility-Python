3b43bec57f0b949fb91ebf55992249426d5c5433
Nattawut Boonnoon <leonardo_90s@hotmail.com>
2026-07-11 14:44:08 +0700

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -13,6 +13,10 @@ Thank you!
 
 
 """
+import os
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
 
 import streamlit as st
 import pandas as pd
